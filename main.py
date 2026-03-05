7809ef6e2a6666d89972d69805ce05760f5c0fe4
DevLoff <nathan.stolzenbach.contact@gmail.com>
2026-03-05 15:46:57 +0100

    main v.01

---
+++ main.py
@@ -0,0 +1,22 @@
+# IMPORT
+from pygame import *
+
+"""
+CONSTANT_CONSTANT : use upper case
+variableVariable : use camel case
+argumentargument : use lower case
+function_function : use snake case
+"""
+
+if __name__ == '__main__':
+    init()
+
+    # CONSTANTS
+    SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+    SCREEN = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # GAME LOOP
+    while True:
+        pass
+
+    quit()
